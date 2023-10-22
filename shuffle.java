bbc1e1aead41738aa765202bf5a6da2da673aeb3
kapilgarg073 <88045962+kapilgarg073@users.noreply.github.com>
2023-10-22 08:21:30 +0530

    Create shuffle.java

---
+++ shuffle.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ShuffleArray {
+    public static void main(String[] args) {
+        List<Integer> arrayList = new ArrayList<>();
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        arrayList.add(4);
+        arrayList.add(5);
+        arrayList.add(6);
+        arrayList.add(7);
+
+        Collections.shuffle(arrayList);
+
+        Integer[] shuffledArray = arrayList.toArray(new Integer[0]);
+
+        for (int value : shuffledArray) {
+            System.out.print(value + " ");
+        }
+    }
+}
