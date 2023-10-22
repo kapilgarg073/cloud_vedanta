c3406ae13cda5544bcb8f47c540075e994c4fd1a
kapilgarg073 <88045962+kapilgarg073@users.noreply.github.com>
2023-10-22 08:31:42 +0530

    calculator

---
+++ calculator/app.js
@@ -0,0 +1,21 @@
+let currentInput = '';
+let display = document.getElementById('display');
+
+function appendToDisplay(value) {
+    currentInput += value;
+    display.value = currentInput;
+}
+
+function clearDisplay() {
+    currentInput = '';
+    display.value = '';
+}
+
+function calculateResult() {
+    try {
+        currentInput = eval(currentInput);
+        display.value = currentInput;
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
